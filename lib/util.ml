1ffbf4819024ed510160162bf5ecb8befe125139
Sarah E. Chasins <schasins@gmail.com>
2023-09-21 12:36:38 -0700

    Status before session on naming expressions (lets).

--- lib/util.ml
+++ lib/util.ml
@@ -4,3 +4,11 @@ let gensym : string -> string =
     let symbol = Printf.sprintf "%s__%d" s !counter in
     counter := !counter + 1 ;
     symbol
+
+module Symtab = Map.Make (struct
+  type t = string
+
+  let compare = compare
+end)
+
+type 'a symtab = 'a Symtab.t
